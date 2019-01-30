72610f5591bde7e81bc300c22797403fd03e38f5
dczeeee <wanghaoyu349@126.com>
2019-01-30 22:34:40 +0800

    1/30

--- 树的子结构/src/树的子结构/HasSubtree.java
+++ 树的子结构/src/树的子结构/HasSubtree.java
@@ -20,6 +20,28 @@ public class HasSubtree {
 
 	}
 	public boolean hasSubtree(TreeNode root1,TreeNode root2) {
-        
+        if(root2==null) return false;
+        if(root1==null && root2!=null) return false;      
+        boolean flag = false;
+        if(root1.val==root2.val){
+            flag = isSubTree(root1,root2);
+        }
+        if(!flag){
+            flag = hasSubtree(root1.left, root2);
+            if(!flag){
+                flag = hasSubtree(root1.right, root2);
+            }
+        }
+        return flag;
+    }
+       
+    private boolean isSubTree(TreeNode root1, TreeNode root2) {
+        if(root2==null) return true;
+        if(root1==null && root2!=null) return false;      
+        if(root1.val==root2.val){
+            return isSubTree(root1.left, root2.left) && isSubTree(root1.right, root2.right);
+        }else{
+        return false;
+        }
     }
 }
