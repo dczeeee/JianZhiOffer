b739b94378a888a3f74c8236925cbbb34c575e71
dczeeee <wanghaoyu349@126.com>
2019-02-24 20:01:57 +0800

    2/24

---
+++ 求1+2+3+...+n/src/Sum.java
@@ -0,0 +1,15 @@
+/*
+ * 题目描述
+求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+ */
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int Sum_Solution(int n) {
+        
+    }
+}
